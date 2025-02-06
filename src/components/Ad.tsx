d8e7c00fa23313743f76d94154521dae8673b03a
ddm479 <ehdtjq5247@gmail.com>
2025-02-06 17:50:15 +0900

    https://creative103.tistory.com/214
    vite에서 react와 typescript실행하는거 더 찾아봐야함함

---
+++ src/components/Ad.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+export const Ad = () => {
+    return (
+        <div>
+            <img src="src\assets\samsungAd.gif"></img>
+        </div>
+    )
+}
+
+export default Ad
