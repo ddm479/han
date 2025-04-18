6b893f1c0fc2838c6db9a369f204ecfb14a1a43a
ddm479 <ehdtjq5247@gmail.com>
2025-04-18 11:27:40 +0900

    모달 컴포넌트 - 모달 창 바깥 영역  클릭 시 닫히는 기능 구현완료 - 추후에 재사용을 위해 className, children 설정정

---
+++ src/components/Modal.tsx
@@ -0,0 +1,43 @@
+// Modal.tsx
+import React, { useEffect, useRef } from "react";
+
+type ModalProps = {
+    isModalOpen: boolean;
+    onClose: () => void;
+    children: React.ReactNode;
+    className?: string;
+};
+
+// 모달 창 바깥 영역 클릭 시 닫히는 효과 구현한 컴포넌트
+const Modal = ({ isModalOpen, onClose, children, className }: ModalProps) => {
+    const modalRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            // 클릭 했을 때 모달 안쪽 영역이 아니라면 닫기
+            if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+                onClose();
+            }
+        };
+
+        // 모달 열렸을 때만 이벤트 등록
+        if (isModalOpen) {
+            document.addEventListener("mousedown", handleClickOutside);
+        }
+
+        // 클린 업
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isModalOpen, onClose]);
+
+    
+
+    return (
+        <div className={className} ref={modalRef}>
+            {children}
+        </div>
+    );
+};
+
+export default Modal;
